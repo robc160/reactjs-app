7a25c90a680779bca384b1b75d272e639ef9428b
Rob Culbertson <rsculbertson@gmail.com>
2018-07-03 00:06:34 -0500

    Example of using container

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import Intro from '../Intro';
+import Series from '../../containers/Series';
 import './App.css';
 import 'whatwg-fetch';
 
 class App extends Component {
-  state = {
-    series: []
-  };
-
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-      .then((response) => response.json())
-      .then(json => this.setState({series: json}))
-  };
+  
   render() {
     return (
       <div className="App">
@@ -20,7 +13,7 @@ class App extends Component {
           <h1 className="App-title">TV Series List</h1>
         </header>
         <Intro message="Here you can find all of your most loved series" />
-        The length of series array: {this.state.series.length}
+        <Series />
       </div>
     );
   }
